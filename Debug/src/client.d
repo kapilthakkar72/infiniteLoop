22b15fe941201a1cf261117b6db3d4fe1cb75def
kapilthakkar72 <kapilthakkar.72@gmail.com>
2015-04-20 22:41:01 +0530

    removed oscillation for p1 in reed opening

---
+++ Debug/src/client.d
@@ -0,0 +1,15 @@
+src/client.d: ../src/client.cpp ../src/Structures.h ../src/Constants.h \
+ ../src/GlobalVariables.h ../src/Helper.h ../src/Utils.h \
+ ../src/Validations.h
+
+../src/Structures.h:
+
+../src/Constants.h:
+
+../src/GlobalVariables.h:
+
+../src/Helper.h:
+
+../src/Utils.h:
+
+../src/Validations.h:
